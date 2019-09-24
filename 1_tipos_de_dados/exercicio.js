1432c956e6b9741095cd415ec1ccd1c33602c7c1
shirleypaz <shirley.pazz@gmail.com>
2019-09-23 22:26:08 -0300

    exercicios

--- 1_tipos_de_dados/exercicio.js
+++ 1_tipos_de_dados/exercicio.js
@@ -1,1 +1,24 @@
 //resposta aqui
+
+let nome = prompt('Insira o nome do cliente');
+let idade = prompt('insira a idade do cliente');
+let primeiraVez = prompt('É a primeira compra?');
+let favoritos = [];
+
+let i = 0;   
+
+while( i < 3 ) {
+    let favorito = promp('Insira um produto favorito');
+    favoritos.push(favorito);
+    i++
+};
+
+
+let cliente = {
+    nome: nome,
+    idade: idade, 
+    primeiraVez: primeiraVez == 'sim' ? true : false,
+    favoritos: favoritos
+}
+
+console.log(cliente, 'cliente')
--- 2_op_ar/exercicio.js
+++ 2_op_ar/exercicio.js
@@ -1,1 +1,24 @@
-//resposta aqui
+var rl = require('readline-sync')
+var produtos = [
+    {
+        nome: 'camiseta',
+        valor: 35
+    },
+    {
+        nome: 'bermuda',
+        valor: 50
+    },
+
+    {
+        nome: 'boné',
+        valor: 20
+    },
+]
+
+var posicao = parseInt(rl.question('Digite a posição do produto (0 a 2)'));
+var desconto = parseInt(rl.question('Digite o desconto'));
+var produtoEscolhido = produtos[posicao];
+var precoFinal = produtoEscolhido.valor * desconto/100 
+var mensagem = `Produto ${produtoEscolhido.nome} com desconto é R$${precoFinal.toFixed(2)}`
+
+console.log(mensagem)
