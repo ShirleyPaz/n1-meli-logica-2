20d71240bfaa97073c9ccbb3b7e765f5ec074d13
shirleypaz <shirley.pazz@gmail.com>
2019-09-25 21:18:40 -0300

    exercicio funcoes

--- 3_estrutura/exercicio.js
+++ 3_estrutura/exercicio.js
@@ -30,14 +30,31 @@ const diasVencimento = parseInt(r.question('Digite quantos dias faltam para venc
 const porcentagem = (diasVencimento * 100) / produtos[posicao].tempoDeValidade;
 const produto = produtos[posicao];
 
-if (porcentagem <= 2) {
-    console.log("Descartar produto, muito proximo de vencer")
-} else if (porcentagem <= 10 && !produto.congelado) {
-    console.log('Desconto!  Produto não congelado e próximo da validade.')
-} else if (porcentagem <= 5 && produto.congelado) {
-    console.log('Desconto!  Produto congelado e próximo da validade.')
-} else if (porcentagem > 10) {
-    console.log('Produto longe do vencimento.');
-} else {
-    console.log('Algo de errado não está certo')
+// if (porcentagem <= 2) {
+//     console.log("Descartar produto, muito proximo de vencer")
+// } else if (porcentagem <= 10 && !produto.congelado) {
+//     console.log('Desconto!  Produto não congelado e próximo da validade.')
+// } else if (porcentagem <= 5 && produto.congelado) {
+//     console.log('Desconto!  Produto congelado e próximo da validade.')
+// } else if (porcentagem > 10) {
+//     console.log('Produto longe do vencimento.');
+// } else {
+//     console.log('Algo de errado não está certo')
+// }
+
+switch (true) {
+    case porcentagem <= 2:
+        console.log("Descartar produto, muito proximo de vencer");
+        break;
+    case porcentagem <= 10 && !produto.congelado:
+        console.log('Desconto!  Produto não congelado e próximo da validade.')
+        break;
+    case porcentagem <= 5 && produto.congelado:
+        console.log('Desconto!  Produto congelado e próximo da validade.')
+        break;
+    case porcentagem > 10:
+        console.log('Produto longe do vencimento.');
+        break;
+    default:
+        console.log('Algo de errado não está certo')
 }
--- 4_funcoes/exercicio.js
+++ 4_funcoes/exercicio.js
@@ -1,1 +1,44 @@
 //resposta aqui
+const r = require('readline-sync')
+
+soma = (v1, v2) => {
+    return v1 + v2
+}
+
+multiplicacao = (v1, v2) => {
+    return v1 * v2
+}
+
+divisao = (v1, v2) => {
+    return v1 / v2;
+}
+
+substracao = (v1, v2) => {
+    return v1 - v2;
+}
+
+const operacao = r.question('Qual operacao deseja fazer?');
+const v1 = parseInt(r.question('Insira o primeiro valor'));
+const v2 = parseInt(r.question('Insira o segundo valor'));
+let res;
+
+switch (operacao) {
+    case 'soma':
+        res = soma(v1, v2);
+        console.log(res)
+        break;
+    case 'subtracao':
+        res = substracao(v1, v2);
+        console.log(res)
+        break;
+    case 'divisao':
+        res = divisao(v1, v2);
+        console.log(res)
+        break;
+    case 'multiplicacao':
+        res = multiplicacao(v1, v2);
+        console.log(res)
+        break;
+    default:
+        console.log("Operação inválida")
+}
